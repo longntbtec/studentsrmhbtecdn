205279c108bb29796b8c4af6a749500f323697c4
Nguyễn Thành Long <longnt69@fpt.edu.vn>
2026-06-02 21:06:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+// Lưu trữ các thông tin cấu hình nhạy cảm hoặc cấu hình môi trường
+const supabaseUrl = 'https://moahaaubepvvpkukjoun.supabase.co';
+// WARNING: Điền Anon Key vào thay cho process.env.SUPABASE_KEY vì file HTML chạy trên trình duyệt client
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYWhhYXViZXB2dnBrdWtqb3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMjk2NTksImV4cCI6MjA5MTkwNTY1OX0.AgY0-QhMeTXF08eCBRnIxyAjq8mtNHYQBDr6F5jqPcQ';
